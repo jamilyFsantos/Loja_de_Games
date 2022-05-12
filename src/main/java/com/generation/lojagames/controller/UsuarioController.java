09b38b3a2c98b6e8cf0ab374e2ee5cd1a00177f5
jamily <jamilyfernandes71@gmail.com>
2022-05-12 20:03:45 -0300

    Exercício

--- src/main/java/com/generation/lojagames/controller/UsuarioController.java
+++ src/main/java/com/generation/lojagames/controller/UsuarioController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,5 +55,15 @@ public class UsuarioController {
 				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> put (@RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		usuarioRepository.deleteById(id);
 	}
 }
